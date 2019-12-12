55e81a72042a8efe13ab401f54c0742a3a24ff54
Govind <abhishek.govind@sap.com>
2019-12-12 14:53:36 +0530

    General Scafolding schema

--- Vision/vision.py
+++ Vision/vision.py
@@ -31,26 +31,26 @@ while True:
     ok, bbox = tracker.update(frame) 
     ## calculation the FPS
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer); 
-    # Draw bounding box
+    ##Drawing the bounding box based of the bbox changes
     if ok:
-        # Tracking success
+        ##Tracking ongoing
         p1 = (int(bbox[0]), int(bbox[1]))
         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
         cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
     else :
-        # Tracking failure
+        ##Tracking? I dont think so
         cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
  
-    # Display tracker type on frame
+    ##Displaying tracker type on frame
     cv2.putText(frame, "GOTURN Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
  
-    # Display FPS on frame
+    ##Displaying  FPS on frame
     cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
  
-    # Display result
+    ## final result display
     cv2.imshow("Tracking", frame)
   
-    # Exit if ESC pressed
+    ##way to exit the screen --> press ESC
     k = cv2.waitKey(1) & 0xff
     if k == 27:
         break
